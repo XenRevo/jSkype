cfb8d1586164624cc9f76d384b87b2c2a4e93580
Ghost <ghost@ghosted.me>
2015-08-26 06:00:40 +0100

    Bug fixes and chat pinger added
    Bug fixes and chat pinger added

--- source/xyz/gghost/jskype/var/Conversation.java
+++ source/xyz/gghost/jskype/var/Conversation.java
@@ -3,6 +3,7 @@ package xyz.gghost.jskype.var;
 import lombok.Getter;
 import lombok.Setter;
 import xyz.gghost.jskype.api.SkypeAPI;
+import xyz.gghost.jskype.internal.packet.packets.PingPrepPacket;
 import xyz.gghost.jskype.internal.packet.packets.SendMessagePacket;
 
 import java.util.ArrayList;
@@ -10,19 +11,20 @@ import java.util.ArrayList;
 /**
  * Created by Ghost on 22/08/2015.
  */
-public class Conversation extends Group{
+public class Conversation extends  Group{
     @Getter @Setter private boolean userChat;
     @Getter @Setter private String id;
     private SkypeAPI api;
     /** Do not use*/
     @Setter @Getter private Group forcedGroupGroup;
     @Getter @Setter private boolean forcedGroup = false;
+    @Getter @Setter private ArrayList<GroupUser> connectedClients = new ArrayList<GroupUser>();
+    @Getter @Setter private String topic = "";
 
     /** This class allows backwards and forwards compatibility of the User and Group class.*/
 
     public Conversation(SkypeAPI api, String id, boolean isGroup) {
-
-        super("", id, null);
+        super(id, "", null);
         try {
             if (isGroup) {
                 this.setConnectedClients(api.getSkype().getGroupById(id).getConnectedClients());
@@ -34,12 +36,19 @@ public class Conversation extends Group{
         this.id = id;
         this.api = api;
     }
-    @Override
+
     public String getTopic(){
         if (!userChat){
             return getGroup().getTopic();
         }
         return "";
+    }
+    public Message sendImageToChat(SkypeAPI api, String URL){
+        if (!userChat) {
+            return new SendMessagePacket(api, api.getSkype()).sendPing(getGroup(), new Message("hi"), new PingPrepPacket(api).urlToId(URL, id));
+        } else {
+            return new SendMessagePacket(api, api.getSkype()).sendPing(id, new Message("hi"), new PingPrepPacket(api).urlToId(URL, id));
+        }
     }
     public Message sendMessage(SkypeAPI api, String text) {
         if (!userChat) {
@@ -55,10 +64,15 @@ public class Conversation extends Group{
             return new SendMessagePacket(api, api.getSkype()).sendMessage(id, text);
         }
     }
+    public void setForcedGroupGroup(Group forcedGroupGroup){
+        this.forcedGroupGroup = forcedGroupGroup;
+    }
     /** If chat is a group, get group or return null*/
     public Group getGroup() {
-        if(forcedGroup)
+        if(forcedGroup){
             return forcedGroupGroup;
+        }
+
         return api.getSkype().getGroupById(id);
     }
     /** If chat is a user, get user or return null*/
